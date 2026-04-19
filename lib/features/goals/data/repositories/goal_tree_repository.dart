2bcfd2d17615b0ddf16cc40dd812320a20f2b171
Fabricio Silva <104510800+Fabrithizio@users.noreply.github.com>
2026-04-19 04:08:01 -0300

    limpeza de arquivos mortos

--- lib/features/goals/data/repositories/goal_tree_repository.dart
+++
@@ -1,9 +0,0 @@
-// lib/features/goals/data/repositories/goal_tree_repository.dart
-import '../models/goal_tree_models.dart';
-
-abstract class GoalTreeRepository {
-  Future<List<GoalSummaryModel>> listGoals();
-  Future<GoalTreeStateModel?> loadGoal(String goalId);
-  Future<void> saveGoal(GoalTreeStateModel state);
-  Future<void> deleteGoal(String goalId);
-}
